2a4ec8c7d29822a6bb558615fecb6e97e8d29531
shohei morita <shomorita1985@gmail.com>
2020-04-07 22:31:02 +0900

    edit作成

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,36 @@
 class ChatsController < ApplicationController
+  before_action :set_chatter, only: [:edit, :update, :destroy]
+  before_action :order_chatters, only: [:index, :create, :confirm]
+
   def index
     @chat = Chat.new
   end
+
+  def create
+    @chat = Chat.new(chat_params)
+    if params[:bacl]
+      render :index
+    else
+      if @chat.save
+        redirect_to chats_path, notice: 'つぶやきを投稿しました！'
+      else
+        render :index
+      end
+    end
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:title, :content)
+  end
+
+  def set_chat
+    @chat = Chat.find(params[:id])
+  end
+
+  def order_chats
+    @chats = Chat.order(id: :DESC)
+  end
+
 end
